f4428c18cef89bd2ef0a97c41495038e0abce057
Yiğit Boran Gülkan <borangulkan@gmail.com>
2025-08-20 10:03:26 +0300

    chore: update dependencies and devDependencies in package.json; add PostCSS configuration for autoprefixer

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,5 +1,6 @@
-const config = {
-  plugins: ["@tailwindcss/postcss"],
+export default {
+  plugins: {
+    "@tailwindcss/postcss": {},
+    autoprefixer: {}, // modern tarayıcı uyumu için
+  },
 };
-
-export default config;
